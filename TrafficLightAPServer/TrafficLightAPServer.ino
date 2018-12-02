d7f561962739e4a19c39c6d16f39dd0b6e9d9d60
Luc-pascal Ceccaldi <lp@ceccaldi.eu>
2018-12-02 00:15:44 +0000

    Added Captive portal with nice default domain name
    Signed-off-by: Luc-Pascal Ceccaldi <lp@ceccaldi.eu>

--- TrafficLightAPServer/TrafficLightAPServer.ino
+++ TrafficLightAPServer/TrafficLightAPServer.ino
@@ -1,9 +1,13 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <ESP8266WebServer.h>
+#include <DNSServer.h>
 
 #define AP_SSID "TrafficLight"
 // AP IP: 192.168.4.1
+
+const IPAddress apIp(192, 168, 4, 1);
+DNSServer dnsServer;
 
 WiFiUDP Udp;
 unsigned int localUdpPort = 4210;  // local port to listen on
@@ -18,13 +22,13 @@ int i = 0;
 
 ESP8266WebServer server(80);//Service Port
 
-void sendACK(){
+void sendACK() {
   // send back a reply, to the IP address and port we got the packet from
   Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
   Udp.write("OK\n");
   Udp.endPacket();
 }
-void sendNACK(){
+void sendNACK() {
   Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
   Udp.write("NOK\n");
   Udp.endPacket();
@@ -39,13 +43,21 @@ void setup()
 
   // must be set to AP, otherwise the STA can become unstable
   // and webserver/DHCP stop responding alltogether
+
+  WiFi.persistent(false);
+  WiFi.disconnect(true);
   WiFi.mode(WIFI_AP);
   Serial.print("Setting soft-AP ... ");
-  if(WiFi.softAP(AP_SSID) == true){
+  WiFi.softAPConfig(apIp, apIp, IPAddress(255, 255, 255, 0));
+  if (WiFi.softAP(AP_SSID) == true) {
     Serial.println("Ready");
-  }else{
+  } else {
     Serial.println("Failed!");
   }
+
+  dnsServer.setErrorReplyCode(DNSReplyCode::NoError);
+  dnsServer.start(53, "*", apIp);
+
   Udp.begin(localUdpPort);
   Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.softAPIP().toString().c_str(), localUdpPort);
 
@@ -57,6 +69,7 @@ void setup()
   server.on("/GON", handleGREENON);
   server.on("/GOF", handleGREENOFF);
   server.on("/ALLOFF", handleALLOFF);
+  server.onNotFound(httpDefault);
 
   server.begin(); // Actually start the server
   Serial.printf("Web server: %s%s\n", "http://", WiFi.softAPIP().toString().c_str());
@@ -65,10 +78,12 @@ void setup()
 void loop()
 {
   int packetSize;
-  static int packetNumber=0;
-  static int nbStationConnected=0;
+  static int packetNumber = 0;
+  static int nbStationConnected = 0;
 
-  if(nbStationConnected != WiFi.softAPgetStationNum()){
+  dnsServer.processNextRequest();
+
+  if (nbStationConnected != WiFi.softAPgetStationNum()) {
     nbStationConnected = WiFi.softAPgetStationNum();
     Serial.printf("Stations connected to soft-AP = %d\n", WiFi.softAPgetStationNum());
   }
@@ -85,41 +100,41 @@ void loop()
     {
       incomingPacket[len] = 0;
     }
-    if(incomingPacket[len-1] == '\n'){ //get rid of '\n' for strcmp
-      incomingPacket[len-1] = 0;
+    if (incomingPacket[len - 1] == '\n') { //get rid of '\n' for strcmp
+      incomingPacket[len - 1] = 0;
     }
     //Serial.printf("UDP packet contents: %s\n", incomingPacket);
-    if(strcmp(incomingPacket, "RON")==0){
+    if (strcmp(incomingPacket, "RON") == 0) {
       Serial.println("RON");
       digitalWrite(redPin, HIGH);
       return;
     }
-    if(strcmp(incomingPacket, "ROF")==0){
+    if (strcmp(incomingPacket, "ROF") == 0) {
       Serial.println("ROF");
       digitalWrite(redPin, LOW);
       return;
     }
-    if(strcmp(incomingPacket, "YON")==0){
+    if (strcmp(incomingPacket, "YON") == 0) {
       Serial.println("YON");
       digitalWrite(yellowPin, HIGH);
       return;
     }
-    if(strcmp(incomingPacket, "YOF")==0){
+    if (strcmp(incomingPacket, "YOF") == 0) {
       Serial.println("YOF");
       digitalWrite(yellowPin, LOW);
       return;
     }
-    if(strcmp(incomingPacket, "GON")==0){
+    if (strcmp(incomingPacket, "GON") == 0) {
       Serial.println("GON");
       digitalWrite(greenPin, HIGH);
       return;
     }
-    if(strcmp(incomingPacket, "GOF")==0){
+    if (strcmp(incomingPacket, "GOF") == 0) {
       Serial.println("GOF");
       digitalWrite(greenPin, LOW);
       return;
     }
-    if(strcmp(incomingPacket, "ALLOFF")==0){
+    if (strcmp(incomingPacket, "ALLOFF") == 0) {
       Serial.println("ALLOFF");
       digitalWrite(redPin, LOW);
       digitalWrite(yellowPin, LOW);
@@ -133,89 +148,96 @@ void loop()
 }
 
 char * linkStr(int pin) {
-    char buf[66] = "<a href=\""; //9
-    if (pin == redPin) {
-        if (digitalRead(pin) == HIGH) {
-            strcat(buf, "ROF\" style=\"background-color:rgba("); // +34
-            strcat(buf, "255,000,000,1.0)\"></a>"); // +22
-        } else {
-            strcat(buf, "RON\" style=\"background-color:rgba("); // +34
-            strcat(buf, "255,000,000,0.1)\"></a>"); // +22
-        }
-    } else if (pin == yellowPin) {
-        if (digitalRead(pin) == HIGH) {
-            strcat(buf, "YOF\" style=\"background-color:rgba("); // +34
-            strcat(buf, "255,165,000,1.0)\"></a>"); // +22
-        } else {
-            strcat(buf, "YON\" style=\"background-color:rgba("); // +34
-            strcat(buf, "255,165,000,0.1)\"></a>"); // +22
-        }
-    } else if (pin == greenPin) {
-        if (digitalRead(pin) == HIGH) {
-            strcat(buf, "GOF\" style=\"background-color:rgba("); // +34
-            strcat(buf, "000,128,000,1.0)\"></a>"); // +22
-        } else {
-            strcat(buf, "GON\" style=\"background-color:rgba("); // +34
-            strcat(buf, "000,128,000,0.1)\"></a>"); // +22
-        }
+  char buf[66] = "<a href=\""; //9
+  if (pin == redPin) {
+    if (digitalRead(pin) == HIGH) {
+      strcat(buf, "ROF\" style=\"background-color:rgba("); // +34
+      strcat(buf, "255,000,000,1.0)\"></a>"); // +22
+    } else {
+      strcat(buf, "RON\" style=\"background-color:rgba("); // +34
+      strcat(buf, "255,000,000,0.1)\"></a>"); // +22
     }
-    return buf;
+  } else if (pin == yellowPin) {
+    if (digitalRead(pin) == HIGH) {
+      strcat(buf, "YOF\" style=\"background-color:rgba("); // +34
+      strcat(buf, "255,165,000,1.0)\"></a>"); // +22
+    } else {
+      strcat(buf, "YON\" style=\"background-color:rgba("); // +34
+      strcat(buf, "255,165,000,0.1)\"></a>"); // +22
+    }
+  } else if (pin == greenPin) {
+    if (digitalRead(pin) == HIGH) {
+      strcat(buf, "GOF\" style=\"background-color:rgba("); // +34
+      strcat(buf, "000,128,000,1.0)\"></a>"); // +22
+    } else {
+      strcat(buf, "GON\" style=\"background-color:rgba("); // +34
+      strcat(buf, "000,128,000,0.1)\"></a>"); // +22
+    }
+  }
+  return buf;
 }
 
+void
+httpDefault()
+{
+  server.sendHeader("Location", "http://trafficlight.pinball/", true);
+  server.send(302, "text/plain", "");
+  server.client().stop();
+}
 
 void handleRoot() {
-    char buf[436] = "<html><head><style>body{display:flex;flex-direction:column;align-items:center;}a{border-radius:50%;font-size:50px;width:30vw;height:30vw;}</style></head><body>"; //159
+  char buf[436] = "<html><head><style>body{display:flex;flex-direction:column;align-items:center;}a{border-radius:50%;font-size:50px;width:30vw;height:30vw;}</style></head><body>"; //159
 
-    strcat(buf, linkStr(redPin)); // +70
-    strcat(buf, linkStr(yellowPin)); // +70
-    strcat(buf, linkStr(greenPin)); // +70
-    strcat(buf, "<a href=\"ALLOFF\" style=\"background-color:black;\"></a>"); // +53
-    strcat(buf, "</body></html>"); // +14
-    server.send(200, "text/html", buf);
+  strcat(buf, linkStr(redPin)); // +70
+  strcat(buf, linkStr(yellowPin)); // +70
+  strcat(buf, linkStr(greenPin)); // +70
+  strcat(buf, "<a href=\"ALLOFF\" style=\"background-color:black;\"></a>"); // +53
+  strcat(buf, "</body></html>"); // +14
+  server.send(200, "text/html", buf);
 }
 
-void handleNotFound(){
-  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
-}
+//void handleNotFound(){
+//  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+//}
 
 // RED
-void handleREDON(){
+void handleREDON() {
   Serial.println("RON");
   digitalWrite(redPin, HIGH);
   handleRoot();
 }
-void handleREDOFF(){
+void handleREDOFF() {
   Serial.println("ROF");
   digitalWrite(redPin, LOW);
   handleRoot();
 }
 
 // YELLOW
-void handleYELLOWON(){
+void handleYELLOWON() {
   Serial.println("YON");
   digitalWrite(yellowPin, HIGH);
   handleRoot();
 }
-void handleYELLOWOFF(){
+void handleYELLOWOFF() {
   Serial.println("YOF");
   digitalWrite(yellowPin, LOW);
   handleRoot();
 }
 
 // GREEN
-void handleGREENON(){
+void handleGREENON() {
   Serial.println("GON");
   digitalWrite(greenPin, HIGH);
   handleRoot();
 }
-void handleGREENOFF(){
+void handleGREENOFF() {
   Serial.println("GOF");
   digitalWrite(greenPin, LOW);
   handleRoot();
 }
 
 // ALLOFF
-void handleALLOFF(){
+void handleALLOFF() {
   Serial.println("ALLOFF");
   digitalWrite(redPin, LOW);
   digitalWrite(yellowPin, LOW);
